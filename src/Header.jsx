e321e7faa446e6957b85b1555f20ec810fbbe9d6
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-12-22 14:07:47 -0500

    Add component files for header and body

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header () {
+    return (
+        <nav>
+            <h1>Gilmore</h1>
+            <div>
+                <p></p>
+                <p>score</p>
+            </div>
+            <div>
+                <p></p>
+                <p>high score</p>
+            </div>
+            <p></p>
+        </nav>
+    )
+}
