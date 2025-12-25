589b1dfb5d78a93965df614eee78598bb26fe323
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-12-25 13:56:59 -0500

    Write card loading functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,51 @@ import { useState, useEffect } from "react";
 function App() {
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
-  const [pokemon, setPokemon] = useState({});
+  const [pokemon, setPokemon] = useState([]);
+  const [cards, setCards] = useState([]);
+
 
   useEffect(() => {
     setHighScore(currScore => Math.max(currScore, score));
   }, [score])
 
-  async function loadCards() {
+  async function loadPokemon() {
     const res = await fetch("https://pokeapi.co/api/v2/pokemon");
     const data = await res.json();
     setPokemon(data.results);
   }
 
   useEffect(() => {
-    loadCards();
+    loadPokemon();
   }, [])
+
+  useEffect(() => {
+    async function loadCard() {
+      if (!pokemon.length) return;
+
+      const cardsArray = await Promise.all(pokemon.map(async (beast) => {
+        const res = await fetch(beast.url);
+        const data = await res.json();
+        return {
+          id: data.id,
+          name: data.name,
+          image: data.sprites["front_default"]
+        }
+      }));
+
+      setCards(cardsArray);
+    }
+    loadCard();
+  }, [pokemon])
+
+  useEffect(() => {
+    console.log(cards);
+  }, [cards])
 
   return (
     <>
       <Header score={score} highScore={highScore} />
-      <CardDeck pokemon={pokemon}/>
+      <CardDeck cards={cards} />
     </>
   )
 }
