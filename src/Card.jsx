440ebe789e4ff3829cc5198d6bbae6798c3c3c2b
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-12-24 20:09:49 -0500

    Write card structure

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,3 +1,8 @@
-export default function Card() {
-    
+export default function Card({ name, image }) {
+    return (
+        <div>
+            <img src={image.src} alt={`Picture of ${name}`} />
+            <p>{name}</p>
+        </div>
+    )
 }
