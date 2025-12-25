acdf5a19723009793fad8213b2e367fbe47b85e2
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2025-12-24 20:05:17 -0500

    Write functionality for updating high score and loading cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
+import Header from "./Header";
+import CardDeck from "./CardDeck";
+import { useState, useEffect } from "react";
 
 function App() {
-  
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    setHighScore(currScore => Math.max(currScore, score));
+  }, [score])
+
+  async function loadCards() {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const data = await res.json();
+    setPokemon(data.results);
+  }
+
+  useEffect(() => {
+    loadCards();
+  }, [])
+
+  return (
+    <>
+      <Header score={score} highScore={highScore} />
+      <CardDeck pokemon={pokemon}/>
+    </>
+  )
 }
 
 export default App
